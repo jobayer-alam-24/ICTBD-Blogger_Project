b5d58f56a02ae721dee623209f427bf5d3cd4e65
jobayer-alam-24 <alamsheikhjobayer2000@gmail.com>
2025-07-10 15:14:34 +0600

    done

--- Projects/BloggerNew/Blogger/Blogger/obj/Debug/net8.0/Blogger.AssemblyInfo.cs
+++ Projects/BloggerNew/Blogger/Blogger/obj/Debug/net8.0/Blogger.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blogger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57bb34e4393cb0143ab062bdacbaa046197a3591")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ca6f4bfa20e1db4d2dabf7ee107c3976e91ecd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blogger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blogger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
